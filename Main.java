c60394c49964e5c8ec1615c6845afd68e38f0d22
Angelostamaria24 <58576057+Angelostamaria24@users.noreply.github.com>
2019-12-06 09:15:33 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,11 +4,32 @@
  * and open the template in the editor.
  */
 package main;
+import java.util.*;
 public class Main {
 
-  public static void main(String[] args) {
+  public static void main(String[] args){
+    Scanner scn=new Scanner(System.in);
     String name="Angelo Sta Maria";
-    System.out.println("Hello World " + name);
+    String Address="Quezon City";
+    
+    
+    System.out.println("Hello World ");
+    System.out.println("What's your name?");
+    name=scn.nextLine();
+   
+    
+    System.out.println("What's your age");
+    int age=scn.nextInt();
+    
+   
+    System.out.println("What's your address?");
+    String address =scn.nextLine();
+    address =scn.nextLine();
+    
+    
+     System.out.println("Hi!"+name);
+     System.out.println("your are "+ age + " years old");
+     System.out.println("you live at " + address );
   }
 
 }
